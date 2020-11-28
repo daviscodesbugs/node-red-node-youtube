cc73f3e451730e51bf674074958bf5acfff43556
daviscodesbugs <daviscodesbugs@gmail.com>
2020-11-28 12:40:24 -0700

    Add a subscription node

--- youtube.js
+++ youtube.js
@@ -15,11 +15,11 @@
  * limitations under the License.
  **/
 
-module.exports = function(RED) {
+module.exports = function (RED) {
     "use strict";
 
     function GoogleAPINode(n) {
-        RED.nodes.createNode(this,n);
+        RED.nodes.createNode(this, n);
         var node = this;
         if (this.credentials &&
             this.credentials.apikey) {
@@ -28,246 +28,327 @@ module.exports = function(RED) {
                 type: "key",
                 key: this.credentials.apikey
             });
-            
-            node.log( "Reauthenticating YouTube API with " + this.credentials.apikey );
+
+            node.log("Reauthenticating YouTube API with " + this.credentials.apikey);
         }
     }
 
-    RED.nodes.registerType("google-config",GoogleAPINode,{
+    RED.nodes.registerType("google-config", GoogleAPINode, {
         credentials: {
-            apikey: { type:"text" }
+            apikey: { type: "text" }
         }
     });
 
-
     function PlayListItemsList(n) {
-        RED.nodes.createNode(this,n);
+        RED.nodes.createNode(this, n);
         this.config = RED.nodes.getNode(n.config);
         var node = this;
         var YoutubeAPI = this.config ? this.config.YoutubeAPI : null;
 
-	    if (!YoutubeAPI) {
+        if (!YoutubeAPI) {
             node.warn("Missing Google credentials");
-            node.status({fill:"red",shape:"ring",text:"Missing Google credentials"});
+            node.status({ fill: "red", shape: "ring", text: "Missing Google credentials" });
             return;
         }
 
         node.status({});
-        node.on("input", function(msg) {
+        node.on("input", function (msg) {
             this.part = n.part || msg.part || "snippet";
             this.playlistId = n.playlistId || msg.playlistId;
             this.playlistItemIds = n.playlistItemIds || msg.playlistItemIds;
             this.maxResults = n.maxResults || msg.maxResults || "5";
-            this.onBehalfOfContentOwner = n.onBehalfOfContentOwner || msg.onBehalfOfContentOwner;    
+            this.onBehalfOfContentOwner = n.onBehalfOfContentOwner || msg.onBehalfOfContentOwner;
             this.pageToken = n.pageToken || msg.pageToken;
             this.videoId = n.videoId || msg.videoId;
             this.fields = n.fields || msg.fields;
-                
+
             var jsonData = { part: this.part };
-            
-            if( this.playlistId )
-                jsonData.playlistId = this.playlistId; 
 
-            if( this.playlistItemIds )
-                jsonData.id = this.playlistItemIds; 
+            if (this.playlistId)
+                jsonData.playlistId = this.playlistId;
 
-            if( this.maxResults ) 
+            if (this.playlistItemIds)
+                jsonData.id = this.playlistItemIds;
+
+            if (this.maxResults)
                 jsonData.maxResults = this.maxResults;
-                
-            if( this.onBehalfOfContentOwner ) 
-                jsonData.onBehalfOfContentOwner = this.onBehalfOfContentOwner;
-                
-            if( this.pageToken ) 
-                jsonData.pageToken =  this.pageToken;
-                            
-            if( this.videoId ) 
-                jsonData.videoId = this.videoId;
-                
-            if( this.fields ) 
-                jsonData.fields = this.fields;
-            
-            node.log( JSON.stringify( jsonData ));            
 
-            node.status({fill:"blue",shape:"dot",text:"Running Query..."});
-            YoutubeAPI.playlistItems.list( jsonData, function(err, data) {
+            if (this.onBehalfOfContentOwner)
+                jsonData.onBehalfOfContentOwner = this.onBehalfOfContentOwner;
+
+            if (this.pageToken)
+                jsonData.pageToken = this.pageToken;
+
+            if (this.videoId)
+                jsonData.videoId = this.videoId;
+
+            if (this.fields)
+                jsonData.fields = this.fields;
+
+            node.log(JSON.stringify(jsonData));
+
+            node.status({ fill: "blue", shape: "dot", text: "Running Query..." });
+            YoutubeAPI.playlistItems.list(jsonData, function (err, data) {
                 if (err) {
                     node.error("failed to fetch YouTube PlaylistItems : " + err);
-                    node.log( JSON.stringify( err ));
-                    
-                    node.status({fill:"red",shape:"ring",text:"error"});
+                    node.log(JSON.stringify(err));
+
+                    node.status({ fill: "red", shape: "ring", text: "error" });
                     return;
                 }
-                
+
                 node.status({});
-    
-                node.status({fill:"green",shape:"dot",text:"processing"});
-                
+
+                node.status({ fill: "green", shape: "dot", text: "processing" });
+
                 msg.topic = "/youtube.v3/playListItemsResponse";
                 msg.payload = data;
                 node.send(msg);
-    
+
                 node.status({});
             });
         });
     }
     RED.nodes.registerType("YouTube PlayListItems", PlayListItemsList);
 
-    
-    
     function PlayListsList(n) {
-        RED.nodes.createNode(this,n);
+        RED.nodes.createNode(this, n);
         this.config = RED.nodes.getNode(n.config);
         var node = this;
         var YoutubeAPI = this.config ? this.config.YoutubeAPI : null;
 
-	    if (!YoutubeAPI) {
+        if (!YoutubeAPI) {
             node.warn("Missing Google credentials");
-            node.status({fill:"red",shape:"ring",text:"Missing Google credentials"});
+            node.status({ fill: "red", shape: "ring", text: "Missing Google credentials" });
             return;
         }
 
         node.status({});
-        node.on("input", function(msg) {
-            	
+        node.on("input", function (msg) {
+
             this.part = n.part || msg.part || "snippet";
             this.playlistId = n.playlistId || msg.playlistId || "";
             this.channelId = n.channelId || msg.channelId || "";
             this.maxResults = n.maxResults || msg.maxResults || "5";
             this.mine = n.mine || msg.mine || "";
-            this.onBehalfOfContentOwner = n.onBehalfOfContentOwner || msg.onBehalfOfContentOwner || "";    
-            this.onBehalfOfContentOwnerChannel = n.onBehalfOfContentOwnerChannel || msg.onBehalfOfContentOwnerChannel || "";    
+            this.onBehalfOfContentOwner = n.onBehalfOfContentOwner || msg.onBehalfOfContentOwner || "";
+            this.onBehalfOfContentOwnerChannel = n.onBehalfOfContentOwnerChannel || msg.onBehalfOfContentOwnerChannel || "";
             this.pageToken = n.pageToken || msg.pageToken || "";
-                
+
             var jsonData = { part: node.part };
-            if( this.playlistId ) 
-                jsonData.id = this.playlistId; 
+            if (this.playlistId)
+                jsonData.id = this.playlistId;
 
-            if( this.channelId ) 
-                jsonData.channelId = this.channelId; 
+            if (this.channelId)
+                jsonData.channelId = this.channelId;
 
-            if( this.maxResults ) 
+            if (this.maxResults)
                 jsonData.maxResults = this.maxResults;
-                
-            if( this.mine ) 
-                jsonData.mine = this.mine;
-                
-            if( this.onBehalfOfContentOwner ) 
-                jsonData.onBehalfOfContentOwner = this.onBehalfOfContentOwner;
-                
-            if( this.onBehalfOfContentOwnerChannel ) 
-                jsonData.onBehalfOfContentOwnerChannel = this.onBehalfOfContentOwnerChannel;
-                
-            if( this.pageToken ) 
-                jsonData.pageToken = this.pageToken;
-                
-            node.log( JSON.stringify( jsonData ));            
 
-            node.status({fill:"blue",shape:"dot",text:"Running Query..."});
-            YoutubeAPI.playlists.list( jsonData, function(err, data) {
+            if (this.mine)
+                jsonData.mine = this.mine;
+
+            if (this.onBehalfOfContentOwner)
+                jsonData.onBehalfOfContentOwner = this.onBehalfOfContentOwner;
+
+            if (this.onBehalfOfContentOwnerChannel)
+                jsonData.onBehalfOfContentOwnerChannel = this.onBehalfOfContentOwnerChannel;
+
+            if (this.pageToken)
+                jsonData.pageToken = this.pageToken;
+
+            node.log(JSON.stringify(jsonData));
+
+            node.status({ fill: "blue", shape: "dot", text: "Running Query..." });
+
+            YoutubeAPI.playlists.list(jsonData, function (err, data) {
                 if (err) {
                     node.error("failed to fetch YouTube Playlists : " + err);
-                    node.log( JSON.stringify( err ));
-                    
-                    node.status({fill:"red",shape:"ring",text:"error"});
+                    node.log(JSON.stringify(err));
+
+                    node.status({ fill: "red", shape: "ring", text: "error" });
                     return;
                 }
-                
+
                 node.status({});
-    
-                node.status({fill:"green",shape:"dot",text:"processing"});
-                
+
+                node.status({ fill: "green", shape: "dot", text: "processing" });
+
                 msg.topic = "/youtube.v3/playListResponse";
                 msg.payload = data;
                 node.send(msg);
-    
+
                 node.status({});
             });
         });
     }
     RED.nodes.registerType("YouTube PlayLists", PlayListsList);
 
-
-    
-    
     function ChannelsList(n) {
-        RED.nodes.createNode(this,n);
+        RED.nodes.createNode(this, n);
         this.config = RED.nodes.getNode(n.config);
         var node = this;
         var YoutubeAPI = this.config ? this.config.YoutubeAPI : null;
 
-	if (!YoutubeAPI) {
+        if (!YoutubeAPI) {
             node.warn("Missing Google credentials");
-            node.status({fill:"red",shape:"ring",text:"Missing Google credentials"});
+            node.status({ fill: "red", shape: "ring", text: "Missing Google credentials" });
             return;
         }
 
         node.status({});
-        node.on("input", function(msg) {
-            	
+        node.on("input", function (msg) {
+
             this.part = n.part || msg.part || "snippet";
             this.categoryId = n.categoryId || msg.categoryId || "";
-            this.forUsername = n.forUsername || msg.forUsername || "";            
+            this.forUsername = n.forUsername || msg.forUsername || "";
             this.channelIds = n.channelIds || msg.channelIds || "";
-            this.managedByMe = n.managedByMe || msg.managedByMe || "";            
-            this.mine = n.mine || msg.mine || "";            
-            this.mySubscribers = n.mySubscribers || msg.mySubscribers || "";                        
+            this.managedByMe = n.managedByMe || msg.managedByMe || "";
+            this.mine = n.mine || msg.mine || "";
+            this.mySubscribers = n.mySubscribers || msg.mySubscribers || "";
             this.maxResults = n.maxResults || msg.maxResults || "5";
-            this.onBehalfOfContentOwner = n.onBehalfOfContentOwner || msg.onBehalfOfContentOwner || "";    
+            this.onBehalfOfContentOwner = n.onBehalfOfContentOwner || msg.onBehalfOfContentOwner || "";
             this.pageToken = n.pageToken || msg.pageToken || "";
-                
+
             var jsonData = { part: node.part };
-            if( this.categoryId ) 
-                jsonData.categoryId = node.categoryId; 
+            if (this.categoryId)
+                jsonData.categoryId = node.categoryId;
 
-            if( this.forUsername ) 
-                jsonData.forUsername = node.forUsername; 
+            if (this.forUsername)
+                jsonData.forUsername = node.forUsername;
 
-            if( this.channelIds ) 
-                jsonData.id = this.channelId; 
+            if (this.channelIds)
+                jsonData.id = this.channelId;
 
-            if( this.managedByMe ) 
-                jsonData.managedByMe = this.managedByMe; 
+            if (this.managedByMe)
+                jsonData.managedByMe = this.managedByMe;
 
-            if( this.mine ) 
-                jsonData.mine = this.mine; 
+            if (this.mine)
+                jsonData.mine = this.mine;
 
-            if( this.mySubscribers ) 
-                jsonData.mySubscribers = this.mySubscribers; 
+            if (this.mySubscribers)
+                jsonData.mySubscribers = this.mySubscribers;
 
-            if( this.maxResults ) 
+            if (this.maxResults)
                 jsonData.maxResults = this.maxResults;
-                
-            if( this.onBehalfOfContentOwner ) 
+
+            if (this.onBehalfOfContentOwner)
                 jsonData.onBehalfOfContentOwner = this.onBehalfOfContentOwner;
-                
-            if( this.pageToken ) 
+
+            if (this.pageToken)
                 jsonData.pageToken = this.pageToken;
 
-            node.log( JSON.stringify( jsonData ));            
+            node.log(JSON.stringify(jsonData));
 
-            
-            node.status({fill:"blue",shape:"dot",text:"Running Query..."});
-            YoutubeAPI.channels.list( jsonData, function(err, data) {
+
+            node.status({ fill: "blue", shape: "dot", text: "Running Query..." });
+            YoutubeAPI.channels.list(jsonData, function (err, data) {
                 if (err) {
                     node.error("failed to fetch YouTube Channels : " + err);
-                    node.log( JSON.stringify( err ));                    
-                    
-                    node.status({fill:"red",shape:"ring",text:"error"});
+                    node.log(JSON.stringify(err));
+
+                    node.status({ fill: "red", shape: "ring", text: "error" });
                     return;
                 }
-                
+
                 node.status({});
-    
-                node.status({fill:"green",shape:"dot",text:"processing"});
-                
+
+                node.status({ fill: "green", shape: "dot", text: "processing" });
+
                 msg.topic = "/youtube.v3/channelListResponse";
                 msg.payload = data;
                 node.send(msg);
-    
+
                 node.status({});
             });
         });
     }
     RED.nodes.registerType("YouTube Channels", ChannelsList);
+
+    function Subscription(n) {
+        RED.nodes.createNode(this, n);
+        this.config = RED.nodes.getNode(n.config);
+        let node = this;
+        let YoutubeAPI = this.config ? this.config.YoutubeAPI : null;
+
+        if (!YoutubeAPI) {
+            node.warn("Missing Google credentials");
+            node.status({
+                fill: "red",
+                shape: "ring",
+                text: "Missing Google credentials"
+            });
+            return;
+        }
+
+        node.status({});
+        node.on("input", (msg) => {
+            // Required =======================================================
+            this.part = n.part || msg.part || "snippet"; // Acceptable: contentDetails, id, snippet, subscriberSnippet
+
+            // exactly one of the following
+            this.channelId = n.channelId || msg.channelId || "";
+            this.subscriptionId = n.subscriptionId || msg.subscriptionId || "";
+            /* NOTE: only possible w/ OAuth
+            this.mine = n.mine || msg.mine || "";
+            this.myRecentSubscribers = n.myRecentSubscribers || msg.myRecentSubscribers || "";
+            this.mySubscribers = n.mySubscribers || msg.mySubscribers || "";
+            */
+
+            // Optional =======================================================
+            this.maxResults = n.maxResults || msg.maxResults || "5"; // Acceptable: 0-50
+            this.order = n.order || msg.order || "relevance"; // Acceptable: alphabetical, relevance, unread
+            this.pageToken = n.pageToken || msg.pageToken || "";
+
+            let json = { part: this.part };
+
+            if (this.channelId)
+                json.channelId = this.channelId;
+            else if (this.subscriptionId)
+                json.subscriptionId = this.subscriptionId;
+
+            if (this.maxResults)
+                json.maxResults = this.maxResults;
+
+            if (this.order)
+                json.order = this.order;
+
+            if (this.pageToken)
+                json.pageToken = this.pageToken;
+
+            node.log(JSON.stringify(json));
+
+            node.status({
+                fill: "blue",
+                shape: "dot",
+                text: "Running Query..."
+            });
+
+            YoutubeAPI.subscriptions.list(json, (err, data) => {
+                if (err) {
+                    node.error("Failed to fetch YouTube Subscriptions: " + JSON.stringify(err));
+                    node.status({
+                        fill: "red",
+                        shape: "ring",
+                        text: "error"
+                    });
+                    return;
+                }
+
+                node.status({
+                    fill: "green",
+                    shape: "dot",
+                    text: "processing"
+                });
+
+                msg.topic = data.kind;
+                msg.payload = data;
+                node.send(msg);
+
+                node.status({});
+            });
+        });
+    }
+    RED.nodes.registerType("YouTube Subscriptions", Subscription)
 
 }
